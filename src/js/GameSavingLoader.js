b90b9f391c7f7ade51678126bce8bd7d8488366d
Andrey Semenov <semenov.orc@gmail.com>
2024-01-26 22:48:33 +0300

    load() worked

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,11 @@
+import read from './reader';
+import json from './parser';
+import GameSaving from './GameSaving';
+
+export default class GameSavingLoader {
+  static load() {
+    return read()
+      .then((data) => json(data))
+      .then((save) => new GameSaving(JSON.parse(save)));
+  }
+}
