a503026ffacd9394136cc7826a2f2d2268bd5dd7
Andrey Semenov <semenov.orc@gmail.com>
2024-01-26 22:54:24 +0300

    add description

--- src/js/GameSaving.js
+++ src/js/GameSaving.js
@@ -1,4 +1,12 @@
+/**
+ * данные персонажа
+ * @class
+ */
 export default class GameSaving {
+  /**
+   *@constructor
+   * @param {object} param0 данные персонажа
+   */
   constructor({ id, created, userInfo }) {
     this.id = id;
     this.created = created;
--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -2,7 +2,17 @@ import read from './reader';
 import json from './parser';
 import GameSaving from './GameSaving';
 
+/**
+ * загружает сохранение,
+ * возвращает экземпляр класса GameSaving
+ * @class
+ */
 export default class GameSavingLoader {
+  /**
+   * загружает сохранение
+   * @static
+   * @returns {object} экземпляр класса GameSaving
+   */
   static load() {
     return read()
       .then((data) => json(data))
